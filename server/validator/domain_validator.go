c079cccd2e81622b0c0a649f20363c4c419f5cd3
lim <huahua1319873800@outlook.com>
2025-04-07 07:34:41 +0000

    ok

--- server/validator/domain_validator.go
+++ server/validator/domain_validator.go
@@ -2,6 +2,7 @@
 package validator
 
 import (
+	"net"
 	"regexp"
 
 	"github.com/go-playground/validator/v10"
@@ -9,16 +10,22 @@ import (
 
 // 初始化字符串相关验证器
 func init() {
-	Register("domain", DomainValidator)
+	Register("domain", DomainOrIPValidator)
 }
 
-// DomainValidator 验证字符串是否为有效的域名
-var DomainValidator validator.Func = func(fl validator.FieldLevel) bool {
-	domain, ok := fl.Field().Interface().(string)
+// DomainOrIPValidator 验证字符串是否为有效的域名或IP地址
+var DomainOrIPValidator validator.Func = func(fl validator.FieldLevel) bool {
+	value, ok := fl.Field().Interface().(string)
 	if !ok {
 		return false
 	}
 
+	// 1. 检查是否为有效的IP地址
+	if ip := net.ParseIP(value); ip != nil {
+		return true
+	}
+
+	// 2. 检查是否为有效的域名
 	// 域名正则表达式
 	// 规则:
 	// 1. 由字母、数字、连字符组成，连字符不能在开头或结尾
@@ -26,5 +33,5 @@ var DomainValidator validator.Func = func(fl validator.FieldLevel) bool {
 	// 3. 至少有一个点，最后一个部分至少2个字符(TLD)
 	domainRegex := regexp.MustCompile(`^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$`)
 
-	return domainRegex.MatchString(domain)
+	return domainRegex.MatchString(value)
 }
